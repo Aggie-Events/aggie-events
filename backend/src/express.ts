74897aa447d45b1d4dfab35bc74bb336b9d5c776
Alex Bui <zanbowie138@gmail.com>
2024-09-16 18:57:10 -0500

    rebase

---
+++ backend/src/express.ts
@@ -0,0 +1,18 @@
+// server.js
+import express from 'express';
+
+const app = express();
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Start the Express server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const cors = require("cors");
+app.use(cors());
+
+export { app };
