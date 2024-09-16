18db35f868f45770a7d7ee47a499bfa1d53e2d8e
Alex Bui <zanbowie138@gmail.com>
2024-09-16 18:44:00 -0500

    working frontend and backend on docker w/o traefik

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,6 +2,7 @@ import { DB } from 'kysely-codegen';
 import { Pool } from 'pg'
 import { Kysely, PostgresDialect} from 'kysely'
 
+
 const dialect = new PostgresDialect({
     pool: new Pool({
         connectionString: process.env.DATABASE_URL,
