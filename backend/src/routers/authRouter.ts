c8e25c793d891dc7b83062fa43290b2a487dc437
Alex Bui <zanbowie138@gmail.com>
2024-10-01 16:33:54 -0500

    test

--- backend/src/routers/authRouter.ts
+++ backend/src/routers/authRouter.ts
@@ -4,7 +4,7 @@ import passport from 'passport';
 export const authRouter = express.Router();
 
 authRouter.get('/user', async (req, res) => {
-    res.send(req.user ? req.user : {} );
+    res.send(req.user ? req.user : null );
 }) 
 
 // Route to start Google OAuth login
