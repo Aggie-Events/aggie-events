bea53b8f14c2a0d46831de04c789cade25d34838
Alex Bui <zanbowie138@gmail.com>
2024-09-30 22:49:22 -0500

    frontend env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ const init = async () => {
 
     const cors = require("cors");
     const corsOptions = {
-        origin: 'http://localhost:3000',  // Allow requests from your frontend's origin
+        origin: process.env.FRONTEND_URL!,  // Allow requests from your frontend's origin
         credentials: true,  // Allow credentials (cookies, session data)
     };
 
