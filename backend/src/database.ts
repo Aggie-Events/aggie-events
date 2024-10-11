3cb7723bf99fa98c68d38f4a31446a12a38b1046
Alex Bui <zanbowie138@gmail.com>
2024-10-11 12:12:03 -0500

    semi-working prod

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -7,10 +7,6 @@ import { Kysely, PostgresDialect} from 'kysely'
 const dialect = new PostgresDialect({
     pool: new Pool({
         connectionString: process.env.DATABASE_URL,
-        database: 'agev',
-        host: 'localhost',
-        user: 'admin',
-        port: 5433,
         max: 10,
     })
 })
