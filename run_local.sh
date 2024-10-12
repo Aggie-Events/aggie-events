636bdd74ae8102b69bf8ebb7879072706688a737
Alex Bui <zanbowie138@gmail.com>
2024-10-12 10:13:49 -0500

    remove unnessecary pruning

--- run_local.sh
+++ run_local.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 IS_RUNNING_TRAEFIK=`docker compose -f compose.deps.yml ps -q traefik`
 IS_RUNNING_DB=`docker compose -f compose.deps.yml ps -q db`
 if [[ "$IS_RUNNING_TRAEFIK" == "" || "$IS_RUNNING_DB" == "" ]]; then
@@ -10,4 +11,3 @@ fi
 
 docker compose -f compose.local.yml down 
 docker compose -f compose.local.yml up --build -d
-docker image prune -af
--- run_prod.sh
+++ run_prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 IS_RUNNING_TRAEFIK=`docker compose -f compose.deps.yml ps -q traefik`
 IS_RUNNING_DB=`docker compose -f compose.deps.yml ps -q db`
 if [[ "$IS_RUNNING_TRAEFIK" == "" || "$IS_RUNNING_DB" == "" ]]; then
@@ -11,4 +12,3 @@ fi
 docker compose -f compose.prod.yml down 
 docker compose -f compose.prod.yml pull
 docker compose -f compose.prod.yml up -d
-docker image prune -af
