44fb745a41965e72634ca35a7cbff71768480bf1
Alex Bui <zanbowie138@gmail.com>
2024-10-11 21:21:32 -0500

    update build to production and run script

--- run_prod.sh
+++ run_prod.sh
@@ -1,2 +1,2 @@
-sudo docker compose -f compose.prod.yml down
-sudo docker compose -f compose.prod.yml up --build -d
+docker compose -f compose.prod.yml down 
+docker compose -f compose.prod.yml up --build -d
