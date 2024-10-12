d2817ea40b28d4b56a644feec16c7ff0bd380249
Alex Bui <zanbowie138@gmail.com>
2024-10-12 10:01:19 -0500

    split deps

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,10 @@
-docker compose -f compose.prod.yml down 
-docker compose -f compose.prod.yml up --build -d
+IS_RUNNING_TRAEFIK=`docker compose -f compose.deps.yml ps -q traefik`
+IS_RUNNING_DB=`docker compose -f compose.deps.yml ps -q db`
+if [[ "$IS_RUNNING_TRAEFIK" == "" || "$IS_RUNNING_DB" == "" ]]; then
+    echo "Traefik or DB is not running. Running now..."
+    docker compose -f compose.deps.yml up -d
+fi
+
+docker compose -f compose.local.yml down 
+docker compose -f compose.local.yml up --build -d
 docker image prune -af
--- run_prod.sh
+++ run_prod.sh
@@ -1,3 +1,10 @@
+IS_RUNNING_TRAEFIK=`docker compose -f compose.deps.yml ps -q traefik`
+IS_RUNNING_DB=`docker compose -f compose.deps.yml ps -q db`
+if [[ "$IS_RUNNING_TRAEFIK" == "" || "$IS_RUNNING_DB" == "" ]]; then
+    echo "Traefik or DB is not running. Running now..."
+    docker compose -f compose.deps.yml up -d
+fi
+
 docker compose -f compose.prod.yml down 
 docker compose -f compose.prod.yml pull
 docker compose -f compose.prod.yml up -d
