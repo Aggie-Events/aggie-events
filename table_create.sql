61b4a897347a8dbbe0e828a1382f62a94ccf402f
Alexander Bui <zanbowie138@gmail.com>
2025-02-18 12:03:58 -0600

    schema update

--- table_create.sql
+++ table_create.sql
@@ -26,7 +26,8 @@ CREATE TABLE users
     user_major       VARCHAR(255)          NULL,
     user_year        INT                   NULL,
     user_description TEXT                  NULL,
-    user_profile_img VARCHAR(255)          NULL
+    user_profile_img VARCHAR(255)          NULL,
+    user_banned      BOOLEAN DEFAULT FALSE NOT NULL
 );
 
 CREATE TABLE tags
@@ -42,18 +43,18 @@ CREATE TYPE event_status AS ENUM (
     'draft',
     'published',
     'cancelled'
-);
+    );
 
 CREATE TABLE events
 (
     event_id          SERIAL PRIMARY KEY,
-    contributor_id    INT                                 NOT NULL,
-    event_name        VARCHAR(255)                        NOT NULL,
-    event_description TEXT                                NULL,
+    contributor_id    INT                                   NOT NULL,
+    event_name        VARCHAR(255)                          NOT NULL,
+    event_description TEXT                                  NULL,
     event_location    VARCHAR(255),
-    event_views       INT       DEFAULT 0                 NOT NULL,
+    event_views       INT         DEFAULT 0                 NOT NULL,
 
-    event_img         VARCHAR(255)                        NULL,
+    event_img         VARCHAR(255)                          NULL,
 
     start_time        TIMESTAMPTZ                           NOT NULL,
     end_time          TIMESTAMPTZ                           NOT NULL,
@@ -65,6 +66,7 @@ CREATE TABLE events
     FOREIGN KEY (contributor_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
+-- This will update the date_modified column to the current time
 CREATE OR REPLACE FUNCTION update_timestamp()
     RETURNS TRIGGER AS
 $$
@@ -74,47 +76,72 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- This will automatically call the update_timestamp() function when events is updated
 CREATE TRIGGER set_timestamp
     BEFORE UPDATE
     ON events
     FOR EACH ROW
 EXECUTE FUNCTION update_timestamp();
 
--- Stores all user offical affiliations
+CREATE TYPE membership_type AS ENUM (
+    -- Any of the below, but can also add/remove other owners and editors, can change organization information
+    'owner',
+    -- Can add/edit events
+    'editor'
+    );
+
+-- Stores all user official affiliations
 CREATE TABLE userorgs
 (
-    user_id INT,
-    org_id  INT,
+    user_id       INT                                   NOT NULL,
+    org_id        INT                                   NOT NULL,
+    role          membership_type,
+    date_created  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    date_modified TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (user_id, org_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
     FOREIGN KEY (org_id) REFERENCES orgs (org_id) ON DELETE CASCADE
 );
 
+-- This will automatically call the update_timestamp() function when userorgs is updated
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE
+    ON userorgs
+    FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+
 -- Stores all of the events that a user saves
 CREATE TABLE savedevents
 (
-    user_id  INT,
-    event_id INT,
+    user_id      INT                                   NOT NULL,
+    event_id     INT                                   NOT NULL,
+    date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (user_id, event_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
     FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE CASCADE
 );
 
+
 -- Stores event org affiliations
 CREATE TABLE eventorgs
 (
-    event_id INT,
-    org_id   INT,
+    event_id INT     NOT NULL,
+    org_id   INT     NOT NULL,
+    -- Users can still associate events with organizations even if they aren't authorized, but it is considered unofficial
+    -- Authorized organization members will still have control over event along with the original poster
+    official BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (event_id, org_id),
     FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE CASCADE,
     FOREIGN KEY (org_id) REFERENCES orgs (org_id) ON DELETE CASCADE
 );
 
+
 -- Stores all tags for each event
 CREATE TABLE eventtags
 (
-    event_id INT,
-    tag_id   INT,
+    event_id INT NOT NULL,
+    tag_id   INT NOT NULL,
     PRIMARY KEY (event_id, tag_id),
     FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE CASCADE,
     FOREIGN KEY (tag_id) REFERENCES tags (tag_id) ON DELETE CASCADE
@@ -123,8 +150,8 @@ CREATE TABLE eventtags
 -- Stores all tags for each org
 CREATE TABLE orgtags
 (
-    org_id INT,
-    tag_id INT,
+    org_id INT NOT NULL,
+    tag_id INT NOT NULL,
     PRIMARY KEY (org_id, tag_id),
     FOREIGN KEY (org_id) REFERENCES orgs (org_id) ON DELETE CASCADE,
     FOREIGN KEY (tag_id) REFERENCES tags (tag_id) ON DELETE CASCADE
@@ -134,16 +161,17 @@ CREATE TABLE orgtags
 CREATE TABLE alternateorgnames
 (
     alternate_name_id SERIAL PRIMARY KEY,
-    org_id            INT,
-    alternate_name    VARCHAR(255),
+    org_id            INT          NOT NULL,
+    alternate_name    VARCHAR(255) NOT NULL,
     FOREIGN KEY (org_id) REFERENCES orgs (org_id) ON DELETE CASCADE
 );
 
 -- Stores what users have liked
 CREATE TABLE userlikes
 (
-    user_id  INT,
-    event_id INT,
+    user_id      INT                                   NOT NULL,
+    event_id     INT                                   NOT NULL,
+    date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (user_id, event_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
     FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE CASCADE
@@ -152,8 +180,9 @@ CREATE TABLE userlikes
 -- Stores where a user has marked that they are attending an event
 CREATE TABLE userattendance
 (
-    user_id  INT,
-    event_id INT,
+    user_id      INT                                   NOT NULL,
+    event_id     INT                                   NOT NULL,
+    date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (user_id, event_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
     FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE CASCADE
@@ -162,8 +191,9 @@ CREATE TABLE userattendance
 -- Stores all user subscriptions
 CREATE TABLE usersubs
 (
-    user_id INT,
-    org_id  INT,
+    user_id      INT                                   NOT NULL,
+    org_id       INT                                   NOT NULL,
+    date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (user_id, org_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
     FOREIGN KEY (org_id) REFERENCES orgs (org_id) ON DELETE CASCADE
@@ -180,13 +210,24 @@ CREATE TYPE friendship_status AS ENUM (
 -- Create the friendships table
 CREATE TABLE friendships
 (
-    id              SERIAL PRIMARY KEY,
-    user1_id        INTEGER REFERENCES users (user_id),
-    user2_id        INTEGER REFERENCES users (user_id),
-    status          friendship_status        DEFAULT 'pending', -- Use the enum type here, and set a default
-    friendship_type VARCHAR(50),
-    created_at      TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (user1_id, user2_id)
+    user1_id     INT                                         NOT NULL,
+    user2_id     INT                                         NOT NULL,
+    status       friendship_status DEFAULT 'pending', -- Use the enum type here, and set a default
+    date_created TIMESTAMPTZ       DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (user1_id, user2_id),
+    FOREIGN KEY (user1_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (user2_id) REFERENCES orgs (org_id) ON DELETE CASCADE
+);
+
+-- Stores user blocks (blocked user's events won't show up in search
+CREATE TABLE blocks
+(
+    user_id      INT                                   NOT NULL,
+    blocked_id   INT                                   NOT NULL,
+    date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (user_id, blocked_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (blocked_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 CREATE TYPE report_status AS ENUM (
@@ -194,20 +235,37 @@ CREATE TYPE report_status AS ENUM (
     'reviewed',
     'resolved',
     'rejected'
-);
+    );
 
 -- Reports table
 CREATE TABLE reports
 (
     report_id         SERIAL PRIMARY KEY,
-    reporter_user_id  INT                                   REFERENCES users (user_id) ON DELETE SET NULL,
-    reported_event_id INT                                   NULL REFERENCES events (event_id) ON DELETE CASCADE,
-    reported_org_id   INT                                   NULL REFERENCES orgs (org_id) ON DELETE CASCADE,
-    report_reason     TEXT                                  NULL,
-    report_date       TIMESTAMPTZ   DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    report_status     report_status DEFAULT 'pending'       NOT NULL,
-    admin_notes       TEXT                                  NULL,
-    resolution_date   TIMESTAMP                             NULL
+    reporter_user_id  INT                                     NULL,
+    reported_event_id INT                                     NULL,
+    reported_org_id   INT                                     NULL,
+    report_reason     TEXT                                    NOT NULL,
+
+    report_status     report_status DEFAULT 'pending'         NOT NULL,
+    admin_notes       TEXT                                    NULL,
+
+    date_created      TIMESTAMPTZ   DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    date_modified     TIMESTAMPTZ   DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    resolution_date   TIMESTAMP                               NULL,
+
+    FOREIGN KEY (reporter_user_id) REFERENCES users (user_id) ON DELETE SET NULL,
+    FOREIGN KEY (reported_event_id) REFERENCES events (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (reported_org_id) REFERENCES orgs (org_id) ON DELETE CASCADE,
+    CHECK (
+        reporter_user_id IS NOT NULL OR reported_event_id IS NOT NULL OR reported_org_id IS NOT NULL
+        )
 );
+
+-- This will automatically call the update_timestamp() function when reports is updated
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE
+    ON reports
+    FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
 
 
