f302412e29614686b8488969f87baec7979780ce
PalaashPandey <palaashpandey2006@gmail.com>
2025-05-30 21:23:04 -0500

    route for fetching all tags

--- backend/src/routers/api-router/tag.ts
+++ backend/src/routers/api-router/tag.ts
@@ -4,6 +4,26 @@ import express from "express";
 export const tagRouter = express.Router();
 
 /**
+ * @route GET /api/tags
+ * @description Fetch all tags from the database
+ * @returns {Object} JSON object containing all tags
+ * @returns {Error} 500 error if tags cannot be fetched
+ */
+tagRouter.get("/", async (req, res) => {
+  try {
+    const tags = await db
+      .selectFrom("tags as t")
+      .orderBy("t.tag_id", "asc")
+      .select("t.tag_name")
+      .execute();
+    res.json({ tags: tags.map((tag: { tag_name: string }) => tag.tag_name) });
+  } catch (error) {
+    console.error("Error fetching tags:", error);
+    res.status(500).send("Error fetching all tags!");
+  }
+});
+
+/**
  * @route GET /api/tags/search
  * @description Search for tags based on query parameters
  * @returns {Object} JSON object containing the search results
